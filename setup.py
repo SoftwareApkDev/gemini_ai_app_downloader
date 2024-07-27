5a035b2363d72725a402fdbb00ad1dc40c2f5b12
GlobalCreativeApkDev <globalcreativeapkdev2022@gmail.com>
2024-07-27 21:46:12 +1200

    Packaged project version 1.9 to PyPi!

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='gemini_ai_app_downloader',
-    version='1.8',
+    version='1.9',
     packages=['gemini_ai_app_downloader'],
     url='https://github.com/SoftwareApkDev/gemini_ai_app_downloader',
     license='MIT',
